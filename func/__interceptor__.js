63cc56090afee78b3184d7887cb09b2dc6b83160
edwagbb <116136084+edwagbb@users.noreply.github.com>
2023-09-20 20:45:46 +0800

    Uploading file /func/__interceptor__.js

--- func/__interceptor__.js
+++ func/__interceptor__.js
@@ -1,21 +1,18 @@
 var fs = require('fs');
+const cloud = require('@sys/cloud');
 
 exports.default = async function (ctx) {
 	try{
 		var funcName = ctx.request.path.match(/^(\/[^\/\\]*?)(\/|$|\?)/)[1];
-		//if(funcName === "/") funcName = "/index";
+		if(funcName === "/") funcName = "/index";
 		return await require('@'+funcName).default(ctx)
 	}catch(e){
-		//return e.message
-		var staticPath = "./static"
-		var file = ctx.request.path.match(/^(\/[^?]*?)(\?|$)/)[1];
-		if(file.indexOf("..")!== -1){
-			return "";
+		return e.message
+		if(ctx.request.url.indexOf("/favicon.ico") === 0) return ""
+		ctx.response.header("content-type",'image/jpeg')
+		if(!cloud.shared.has('404_jpg')){
+			cloud.shared.set('404_jpg',(await cloud.fetch("https://im.gurl.eu.org/file/99631c17549f3c02b9dc2.jpg",{ responseType: 'arraybuffer' })).data)
 		}
-		if(file[file.length-1] === "/") file += 'index.html';
-		if(/\.js$/i.test(file)){
-			ctx.response.setHeader('content-type','text/javascript')
-		}
-		return fs.createReadStream(staticPath+file)
+		return cloud.shared.get('404_jpg')
 	}
 }
